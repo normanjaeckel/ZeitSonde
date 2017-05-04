3d9c8c5c2d8595e3e6437b4ddc21b0e8074dcc83
Norman Jäckel <zeitsonde@normanjaeckel.de>
2017-05-04 23:55:15 +0200

    Fixed logging.

--- server/runner.py
+++ server/runner.py
@@ -1,24 +1,22 @@
 
 import logging
 import os
-import sys
 
+import txaio
 from autobahn.asyncio.wamp import ApplicationRunner
 
 from .component import ZeitSonde
-
-logging.basicConfig(level=int(os.environ.get(
-    'ZEITSONDE_LOGLEVEL',
-    logging.DEBUG
-)))
-logger = logging.getLogger(__name__)
 
 
 def run():
     """
     Main entry point for ZeitSonde server application.
     """
-    logger.info('Starting ZeitSonde server')
+    txaio.start_logging(level=os.environ.get(
+        'ZEITSONDE_LOGLEVEL',
+        'info'
+    ))
+    logging.info('Starting ZeitSonde server')
 
     runner_kwargs = {
         'url': os.environ.get(
@@ -29,9 +27,6 @@ def run():
             'ZEITSONDE_WAMP_REALM',
             'realm1'
         ),
-        'extra': {
-            'logger': logger,
-        },
     }
     runner = ApplicationRunner(**runner_kwargs)
     runner.run(ZeitSonde)
