d646f00418e6403c937d53220e4e6fdfeaf569f1
Norman Jäckel <zeitsonde@normanjaeckel.de>
2017-05-05 01:15:28 +0200

    Draft for data schema.

--- server/component.py
+++ server/component.py
@@ -5,10 +5,40 @@ from autobahn.asyncio.wamp import ApplicationSession
 
 class ZeitSonde(ApplicationSession):
     """
-    TODO
+    ZeitSonde application.
     """
+    root_uri = 'zeitsonde'
+
     async def onJoin(self, details):
         logging.info('ZeitSonde server has joined to WAMP router')
+        await self.register(self.get_data(), '{}.get_data'.format(self.root_uri))
 
-        import asyncio
-        await asyncio.sleep(3)
+    def get_data(self):
+        """
+        TODO
+        """
+        logging.debug('ZeitSonde.get_data() called')
+        return [{'all': 'data'}]
+
+
+# {'id': '3240',
+#  'description': 'foo bar baz',
+#  'records': [
+#     {'description': 'Work on xyz',
+#      'start': 'timestamp',
+#      'end': 'timestamp',
+#      'interruption': 'timedelta'
+#     },
+#     {'description': 'Work on something else',
+#      'start': 'timestamp',
+#      'end': 'timestamp',
+#      'interruption': 'timedelta'
+#     },
+#  ],
+#  'extra_time': [
+#     {'description': '...',
+#      'estimated_time': '...',
+#      'duration': 'timedelta',
+#     }
+#  ],
+# }
